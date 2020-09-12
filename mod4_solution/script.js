6e1c25d6a5e6ea86975b2b48252adf47acf0a6d6
lucasdamianjohnson <60717791+lucasdamianjohnson@users.noreply.github.com>
2020-09-12 15:29:37 -0400

    Update script.js

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -52,7 +52,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // See Lecture 50, part 1
 for (var i = 0; i < names.length; i++) {
 
-      var name = names[i];
+      var name = names[i].toLowerCase();
 
 
 
@@ -72,7 +72,7 @@ for (var i = 0; i < names.length; i++) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if ( name[0].toLocaleLowerCase() == "j") {
+  if ( name[0] == "j") {
      byeSpeaker.speak(name);
   } else {
      helloSpeaker.speak(name);
