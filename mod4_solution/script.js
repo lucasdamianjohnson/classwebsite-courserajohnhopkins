01b6408fac3314366c4bb902ae4a6b5963f202da
lucasdamianjohnson <60717791+lucasdamianjohnson@users.noreply.github.com>
2020-09-12 15:26:27 -0400

    Update script.js

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -72,10 +72,10 @@ for (var i = 0; i < names.length; i++) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (true) {
-    // byeSpeaker.xxxx
+  if (if name[0].toLocaleLowerCase() == "j") {
+     byeSpeaker.speak(name);
   } else {
-    // helloSpeaker.xxxx
+     helloSpeaker.speak(name);
   }
 }
 
